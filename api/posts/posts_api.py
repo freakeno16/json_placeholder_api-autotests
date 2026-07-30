7d4c6439b780070b4046f2abcc0da6d0d22519f0
Nikita <freakeno16@ya.ru>
2026-07-30 15:59:56 +0500

    chore: prepared PostsAPI class

---
+++ api/posts/posts_api.py
@@ -0,0 +1,11 @@
+import requests
+from api.base_api import BaseAPI
+
+
+class PostsAPI(BaseAPI):
+    def __init__(self):
+        super().__init__()
+        self.endpoint = '/posts'
+
+    def get_all_posts(self):
+        return requests.get(f'{self.base_url}{self.endpoint}')
